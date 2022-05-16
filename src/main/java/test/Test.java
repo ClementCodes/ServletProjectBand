74c86d6dd7c016cfcda1fecd333fb3f3ae585786
François-Nicolas <francoisnicolas.mm@gmail.com>
2022-05-16 14:46:33 +0200

    Maj jre

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -2,7 +2,9 @@ package main.java.test;
 
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println("coucou");
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("j'en ai marre !!!!!!!!!!!!!");
+	}
+
 }
